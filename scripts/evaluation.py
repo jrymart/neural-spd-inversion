c8232977f11ea6d9dfabdc2e3250f4ed1d1c8bfc
Jo Martin <jo.ryder.martin@gmail.com>
2026-03-03 15:20:56 -0500

    fixing tangling

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -2,8 +2,6 @@ import torch
 from neural_spd.train_peclet_model import PecletModelTrainer
 from neural_spd.ThreeLayerCNNRegressor import ThreeLayerCNNRegressor, JumboThreeLayerCNNRegressor
 import json
-import os
-from pathlib import Path
 
 from neural_spd.config import MODEL_STATS_PATH, DB_PATH, MODEL_DEM_PATH, MODEL_SLOPE_PATH, MODEL_ACC_PATH, MODEL_CURV_PATH, WEIGHTS_PATH, NN_SEEDS, NUM_EPOCHS, LEARNING_RATE, RETRAIN_MODELS, NOISE_LEVELS, DATA_TYPES, LABELS, DATA_PATH, LOG_PATH, IS_HEADLESS, CHECKPOINT_PATH, BATCH_SIZE
 # Use environment variables if set (for HPC scratch filesystem)
@@ -17,6 +15,29 @@ from itertools import product
 
 with open(MODEL_STATS_PATH, 'r') as f:
     statistics = json.load(f)
+
+def eval_neural_net(seed, noise, data_type, label):
+    label_key, label_query = label
+    torch.manual_seed(seed)
+    weights_path = WEIGHTS_PATH / f"n{noise.replace('.','_')}_{data_type}_{seed}_{label_key}_weights.pt"
+    weights_path = RESULTS_DIR / f"n{noise.replace('.','_')}_{data_type}_{seed}_{label_key}_results.csv"
+    dataset_path = DATA_PATH / str(noise).replace('.','_') / data_type
+    if not os.path.exists(dataset_path) or RETRAIN_MODELS:
+        print(f"evaluating {weights_path}")
+        trainer = PecletModelTrainer(DB_PATH,
+                                    data['data_path'],
+                                    ThreeLayerCNNRegressor(),
+                                    LABEL_QUERY,
+                                    epochs = NUM_EPOCHS,
+                                    learning_rate = LEARNING_RATE,
+                                    **statistics[data['type']],
+                                    **statistics['labels'])
+        trainer.load_weights(weights_path)
+        trainer.evaluate()
+        csv_path = RESULTS_DIR / f"{data['type']}_{seed}_results.csv")
+        trainer.test_df.to_csv(csv_path)
+    else:
+        print(f"{weights_path} already evaluated, skipping")
 
 runs = list(product(NN_SEEDS, NOISE_LEVELS, DATA_TYPES, LABELS.items()))
 if IS_HEADLESS:
